da649a6684973772ff4ba3510304da9534c6d5ae
home <home@admins-MacBook-Pro.local>
2024-01-31 18:12:11 +0100

    CV after GWO

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,197 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import portrait from "../src/assets/images/portrait.jpg";
+import personalIcon from "../src/assets/images/icons/personal-icon.svg";
+import languagesIcon from "../src/assets/images/icons/languages-icon.svg";
+import technologiesIcon from "../src/assets/images/icons/technologies-icon.svg";
+import hobbyIcon from "../src/assets/images/icons/hobby-icon.svg";
+import bikeIcon from "../src/assets/images/icons/bike-icon.svg";
+import mathIcon from "../src/assets/images/icons/math-icon.svg";
+import cookingIcon from "../src/assets/images/icons/cooking-icon.svg";
+import skillsIcon from "../src/assets/images/icons/skills-icon.svg";
+import workHistoryIcon from "../src/assets/images/icons/work-history-icon.svg";
+import educationIcon from "../src/assets/images/icons/education-icon.svg";
+import { myInfo } from "./myInfo";
+
+const hobbyIcons: { [key: string]: string } = {
+  bike: bikeIcon,
+  math: mathIcon,
+  cooking: cookingIcon,
+};
 
 function App() {
+  const {
+    name,
+    description,
+    personalInfo,
+    languagesInfo,
+    technologiesInfo,
+    hobbyInfo,
+    skillsInfo,
+    workHistoryInfo,
+    educationInfo,
+    clause,
+  } = myInfo;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"print-area"}>
+      <div className={"portrait"}>
+        <div className={"image-container"}>
+          <img src={portrait} alt="portrait" />
+        </div>
+      </div>
+
+      <div className={"header"}>
+        <div className={"name"}>{name}</div>
+        <div className={"description"}>{description}</div>
+      </div>
+
+      <div className="additional-info">
+        <div className="personal-info">
+          <div className="part-header">
+            <img src={personalIcon} alt="personal icon" />
+            <p>personal info</p>
+          </div>
+
+          {personalInfo.map((info) => {
+            return (
+              <div className="item">
+                <div className="personal-info-name">{info.name}</div>
+                <div className="personal-info-details">{info.details}</div>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="languages">
+          <div className="part-header">
+            <img src={languagesIcon} alt="languages icon" />
+            <p>languages</p>
+          </div>
+
+          {languagesInfo.map((info) => {
+            return (
+              <div className="item">
+                <b>{info.language}:</b>&nbsp;&nbsp;&nbsp;{info.level}
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="technologies">
+          <div className="part-header">
+            <img src={technologiesIcon} alt="technologies icon" />
+            <p>technologies</p>
+          </div>
+
+          <div className="technologies-items">
+            {technologiesInfo.map((technology) => {
+              return <div className="item">{technology}</div>;
+            })}
+          </div>
+        </div>
+
+        <div className="hobby">
+          <div className="part-header">
+            <img src={hobbyIcon} alt="hobby icon" />
+            <p>hobby</p>
+          </div>
+
+          {hobbyInfo.map((hobby) => {
+            return (
+              <div className="item">
+                <img src={hobbyIcons[hobby.iconName]} alt="hobby icon" />
+                <p>{hobby.name}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="main-info">
+        <div className="skills">
+          <div className="part-header">
+            <img src={skillsIcon} alt="skills icon" />
+            <p>skills</p>
+          </div>
+
+          {skillsInfo.map((skill) => {
+            return (
+              <div className="item">
+                <div className="dot"></div>
+                <div className="line"></div>
+                <p>{skill}</p>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="work-history">
+          <div className="part-header">
+            <img src={workHistoryIcon} alt="work history icon" />
+            <p>work history</p>
+          </div>
+
+          {workHistoryInfo.map((work) => {
+            return (
+              <div className="item">
+                <div className="row">
+                  <div className="period">
+                    {work.start} -<br />
+                    {work.end}
+                  </div>
+                  <div className="dot"></div>
+                  <div className="line"></div>
+                  <p className="position">{work.position}</p>
+                </div>
+                <div className="row">
+                  <div className="line"></div>
+                  <p className="employer">
+                    {work.employer}, {work.city}
+                  </p>
+                </div>
+                {work.details?.map((detail) => (
+                  <div className="row">
+                    <div className="line"></div>
+                    <p className="work-details">- {detail}</p>
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="education">
+          <div className="part-header">
+            <img src={educationIcon} alt="education icon" />
+            <p>education</p>
+          </div>
+
+          {educationInfo?.map((education) => (
+            <div className="item">
+              <div className="row">
+                <div className="period">
+                  {education.start} -<br />
+                  {education.end}
+                </div>
+                <div className="dot"></div>
+                <div className="line"></div>
+                <p className="field">
+                  {education.field}, {education.grade}
+                </p>
+              </div>
+              <div className="row">
+                <div className="line"></div>
+                <p className="school">{education.school}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="footer">
+        <p>{clause}</p>
+      </div>
     </div>
   );
 }
---
+++ src/myInfo.ts
@@ -0,0 +1,85 @@
+export const myInfo = {
+  title: "cv",
+
+  clause:
+    "I hereby consent to the processing of my personal data for the purposes of this process and future recruitments (in accordance with the Act of May 10, 2018 on the protection of personal data (Journal Act of 2018, item 1000) and in accordance with the Regulation of the European Parliament and of the Council (EU) 2016/679 of 27 April 2016 on the protection of individuals with regard to data processing personal data and on the free movement of such data and repealing Directive 95/46 / EC (GDPR)).",
+  name: "Żaneta Czapiewska",
+  portraitUrl: "../assets/images/portrait.jpg",
+  description:
+    "I am well organised, persistent, conscientious and patient person. I like to cooperate with other persons. I also like to develop myself and learn new skills, especially in the programming field.",
+  personalInfo: [
+    { name: "email", details: "zancza@gmail.com" },
+    { name: "phone", details: "692 202 262" },
+    { name: "github", details: "www.github.com/zylka44" },
+    { name: "linkedin", details: "www.linkedin.com/in/zanetaczapiewska" },
+  ],
+  languagesInfo: [{ language: "English", level: "intermediate" }],
+  technologiesInfo: [
+    "JavaScript",
+    "TypeScript",
+    "HTML",
+    "SCSS",
+    "Angular",
+    "RxJS",
+    "NgRx",
+    "React",
+    "Redux",
+    "Git",
+    "Node.js",
+    "Visual Studio Code",
+    "Web Storm",
+    "Adobe Animate",
+    "Jira",
+  ],
+  hobbyInfo: [
+    { name: "bike traveling", iconName: "bike" },
+    { name: "mathematics", iconName: "math" },
+    { name: "cooking", iconName: "cooking" },
+  ],
+
+  skillsInfo: [
+    "good knowledge of JavaScript and TypeScript",
+    "working knowledge of HTML5, CSS3, Saas",
+    "development experience in Angular",
+    "experience in React and Redux",
+    "experience in RxJS and NgRx",
+    "practical knowledge of Git",
+    "team spirit and communication skills",
+  ],
+  workHistoryInfo: [
+    {
+      position: "JavaScript developer",
+      employer: "Gdańskie Wydawnictwo Oświatowe",
+      city: "Gdańsk",
+      start: "September 2019",
+      end: "at present",
+      details: [
+        "programming educational interactive games using TypeScript",
+        "building web applications in Angular",
+      ],
+    },
+    {
+      position: "mathematics and computer science teacher",
+      employer: "Primary School",
+      city: "Pruszcz Gdański",
+      start: "September 2011",
+      end: "August 2019",
+    },
+    {
+      position: "mathematics teacher",
+      employer: "Art School",
+      city: "Gdynia",
+      start: "January 2011",
+      end: "July 2011",
+    },
+  ],
+  educationInfo: [
+    {
+      field: "mathematics with computer science",
+      grade: "master",
+      school: "University of Gdańsk",
+      start: "October 2006",
+      end: "July 2011",
+    },
+  ],
+};
